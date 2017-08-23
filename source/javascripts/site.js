81d937203388477fb1782aabc6cd83f2f63552d1
noda <noda@haw.co.jp>
2017-08-23 14:56:01 +0900

    :sparkles: Beautify HTML/CSS/JavaScript

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,18 @@
-// This is where it all goes :)
-$(function(){
-  var pageTop = $("#page_top_button");
-  pageTop.hide();
-  pageTop.click(function () {
+$(() => {
+  const $window = $(window);
+  const $pageTop = $('#page_top_button');
+
+  $pageTop.hide();
+  $pageTop.click(() => {
     $('body, html').animate({ scrollTop: 0 }, 500);
     return false;
   });
-  $(window).scroll(function () {
-    if($(this).scrollTop() >= 20) {
-      pageTop.fadeIn();
+
+  $window.scroll(() => {
+    if ($window.scrollTop() >= 20) {
+      $pageTop.fadeIn();
     } else {
-      pageTop.fadeOut();
+      $pageTop.fadeOut();
     }
   });
 });
