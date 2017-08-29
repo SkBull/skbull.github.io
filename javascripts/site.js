053ad18d3e36ca1ee06d19172a9861e566dc0b59
My Name <you@example.com>
2017-08-29 15:03:19 +0900

    Automated commit at 2017-08-29 06:03:19 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,42 @@
+// 一番上に行けるボタン
+$(() => {
+  const $window = $(window);
+  const $pageTop = $('.page-top-button');
+
+  $pageTop.hide();
+  $pageTop.click(() => {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
+  $window.scroll(() => {
+    if ($window.scrollTop() >= 20) {
+      $pageTop.fadeIn();
+    } else {
+      $pageTop.fadeOut();
+    }
+  });
+
+  // 一番下に行けるボタン
+  const $window = $(window);
+  const $pageBottom = $('.page-bottom-button');
+
+  $pageBottom.hide();
+  $pageBottom.click(() => {
+    $('body, html').animate({ scrollTop: $(document).height() }, 500);
+    return false;
+  });
+
+  $window.scroll(() => {
+    if ($window.scrollTop() >= 50) {
+      $pageBottom.fadeOut();
+    } else {
+      $pageBottom.fadeIn();
+    }
+  });
+
+  $('.menu').click(() => {
+    $('.pages-nav')
+      .toggleClass('is-active');
+    });
+});
